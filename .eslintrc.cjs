8d242f8717b8052898629fddb74c147926191b63
Matthew Lonis <mattlonis@gmail.com>
2023-02-13 16:10:26 -0600

    Updating dependencies

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,15 +1,19 @@
 /* eslint-env node */
-require('@rushstack/eslint-patch/modern-module-resolution');
+// require('@rushstack/eslint-patch/modern-module-resolution');
 
 module.exports = {
   root: true,
-  extends: [
-    'plugin:vue/vue3-essential',
-    'eslint:recommended',
-    '@vue/eslint-config-typescript',
-    '@vue/eslint-config-prettier'
-  ],
+  extends: [],
   overrides: [
+    {
+      files: ['src/**/*.{js,ts,jsx,tsx,vue}'],
+      extends: [
+        'plugin:vue/vue3-essential',
+        'eslint:recommended',
+        '@vue/eslint-config-typescript',
+        '@vue/eslint-config-prettier'
+      ]
+    },
     {
       files: ['cypress/e2e/**/*.{cy,spec}.{js,ts,jsx,tsx}'],
       extends: ['plugin:cypress/recommended']
